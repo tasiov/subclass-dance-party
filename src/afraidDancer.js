51de817c1f4b7f0a503f7325984d434d87433ae2
tasiov <tasiovictoria@gmail.com>
2016-01-18 17:34:34 -0800

    Added a new dancer class which responds to mouse position and moves away

--- src/afraidDancer.js
+++ src/afraidDancer.js
@@ -12,50 +12,36 @@ makeAfraidDancer.prototype.constructor = makeAfraidDancer;
 
 makeAfraidDancer.prototype.step = function() {
     var that = this;
-    var itemOffset = that.$node.offset();
+    // var itemOffset = that.$.offset();
+    var itemTop = that.top;
+    var itemLeft = that.left;
+
 
     makeDancer.prototype.step.call(this);
 
     $( 'body' ).mousemove(function( event ) {
       var msg = "Handler for .mousemove() called at ";
       msg += event.pageX + ", " + event.pageY;
-      console.log(msg);
+      // console.log(msg);  
+      checkMouse(event.pageX, event.pageY);
     });
+
+    var top = itemTop + 200;
+    var left = itemLeft+ 200;
+    var height = $(window).height();
+    var width = $(window).width();
+
+    top = top > height || top < 0 ? Math.round(height/2) : top;
+    left = left > width || left < 0 ? Math.round(width/2) : left;
  
-    function checkOtherDancers() {
-      _.each(window.dancers, function(jqNode) {
-        var offset = jqNode.offset();
-        var diffTop = itemOffset.top - offset.top;
-        var moveTop;
-        var moveLeft;
-        if (diffTop) {
-          var diffLeft = itemOffset.left - offset.left;
-          if (Math.abs(diffTop) <= 200 && Math.abs(diffLeft) <= 200){
-            if (diffTop < 0){
-              moveTop = -250;
-            } else if (diffTop > 0){
-              moveTop = 250;
-            }
-            if (diffLeft < 0){
-              moveLeft = - Math.random()*250;
-            } else if (diffLeft > 0){
-              moveLeft = Math.random()*250;
-            }
-            var top = offset.top + moveTop;
-            var left = offset.left+ moveLeft;
-            var height = $(window).height();
-            var width = $(window).width();
-
-            top = top > height|| top < 0 ? Math.round(height/2) : top;
-            left = left > width || left < 0 ? Math.round(width/2) : left;
-
-            that.$node.animate({
+    function checkMouse(mouseX, mouseY) {
+      if (Math.abs(mouseX - itemLeft) < 50 && Math.abs(mouseY - itemTop) < 50){
+        console.log("animate");
+        console.log("top" + top);
+         that.$node.animate({
               top: "" + top,
               left: "" + left
-            }, 2000);
-          }
-        }
-      });
+            });
+      }
     }
-    // checkOtherDancers();
 };
